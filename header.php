5130f9b0a0152726bb31c4c72f0a17f85ed85c24
Tiago Oliveira <otw1984@gmail.com>
2015-05-29 08:50:49 -0300

    Primeiro commit

---
+++ header.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-inverse">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    
+    <a class="navbar-brand" href="home">PROJETO Otw1984</a>
+    
+    </div>
+    
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+      <!--<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>-->
+      <li><a href="home">HOME</a></li>
+      <li><a href="empresa">EMPRESA</a></li>
+      <li><a href="produtos">PRODUTOS</a></li>
+      <li><a href="servicos">SERVIÇOS</a></li>
+      <li><a href="contato">CONTATO</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</nav>
---
+++ produtos.php
@@ -0,0 +1,77 @@
+<div class="container">
+  <!-- Title -->
+  <div class="row">
+    <div class="col-lg-12">
+      <h3>Produtos</h3>
+    </div>
+  </div>
+
+  <!--### PRODUTOS ###-->
+  <div class="row text-center">
+    <div class="col-md-3 col-sm-6 hero-feature">
+      <div class="thumbnail">
+        <img src="http://placehold.it/800x500" alt="">
+        <div class="caption">
+          <h3>Feature Label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <p><a href="#" class="btn btn-primary">Comprar</a> <a href="#" class="btn btn-default">Detalhes</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 hero-feature">
+      <div class="thumbnail">
+        <img src="http://placehold.it/800x500" alt="">
+        <div class="caption">
+          <h3>Feature Label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <p><a href="#" class="btn btn-primary">Comprar</a> <a href="#" class="btn btn-default">Detalhes</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 hero-feature">
+      <div class="thumbnail">
+        <img src="http://placehold.it/800x500" alt="">
+        <div class="caption">
+          <h3>Feature Label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <p><a href="#" class="btn btn-primary">Comprar</a> <a href="#" class="btn btn-default">Detalhes</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 hero-feature">
+      <div class="thumbnail">
+        <img src="http://placehold.it/800x500" alt="">
+        <div class="caption">
+          <h3>Feature Label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <p><a href="#" class="btn btn-primary">Comprar</a> <a href="#" class="btn btn-default">Detalhes</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 hero-feature">
+      <div class="thumbnail">
+        <img src="http://placehold.it/800x500" alt="">
+        <div class="caption">
+          <h3>Feature Label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <p><a href="#" class="btn btn-primary">Comprar</a> <a href="#" class="btn btn-default">Detalhes</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 col-sm-6 hero-feature">
+      <div class="thumbnail">
+        <img src="http://placehold.it/800x500" alt="">
+        <div class="caption">
+          <h3>Feature Label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <p><a href="#" class="btn btn-primary">Comprar</a> <a href="#" class="btn btn-default">Detalhes</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
