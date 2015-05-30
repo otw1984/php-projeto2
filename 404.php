162d362cf05c86c08b095573a9ebd729095cc3db
Tiago Oliveira <otw1984@gmail.com>
2015-05-30 11:06:02 -0300

    Correcao do arquivo 404.php

---
+++ 404.php
@@ -0,0 +1,10 @@
+<div class="container">
+  <div class="col-lg-8 col-lg-offset-2 text-center">
+    <div class="logo">
+      <h1>OPPS, Erro 404 !</h1>
+      <p class="text-muted">A página que está tentando acessar não existe.</p>
+      <p class="text-muted">Por favor acesse uma página válida. </p>
+    </div>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+  </div>
+</div>
